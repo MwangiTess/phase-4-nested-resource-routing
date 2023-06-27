47a0df6713d211d0c0c804185bf8c754b2f3bbf2
Teresia <teresiamwangi26@gmail.com>
2023-06-27 20:56:36 +0300

    Complete

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -1,6 +1,11 @@
 class DogHousesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  def index 
+    dog_house = DogHouse.all 
+    render json:dog_house
+  end
+
   def show
     dog_house = DogHouse.find(params[:id])
     render json: dog_house
